88a22a833b965a346a975d3c58b2ea60e07fdae0
s.kres <agerest@bk.ru>
2019-11-03 00:19:59 +0400

    fixed registration

--- src/main/java/com/beerasta/config/WebConfig.java
+++ src/main/java/com/beerasta/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.beerasta.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -13,5 +14,10 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
     }
 }
